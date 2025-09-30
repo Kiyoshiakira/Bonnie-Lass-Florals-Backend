28dcd2e8172fbd824b365fcbb939ef0c5f5be9d2
Kiyoshiakira <shaunessy24@gmail.com>
2025-09-30 08:50:21 -0500

    Delete routes/Social.js

--- routes/Social.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const socialSchema = new mongoose.Schema({
-  platform: String,
-  url: String
-});
-
-module.exports = mongoose.model('Social', socialSchema);
