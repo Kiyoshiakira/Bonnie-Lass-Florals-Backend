6e6d4c9f6f18b39738248dd2252522d93e463baf
Kiyoshiakira <shaunessy24@gmail.com>
2025-09-30 11:33:27 -0500

    Add payments route to the API

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.use('/api/contact', require('./routes/contact'));
 app.use('/api/products', require('./routes/products'));
 app.use('/api/orders', require('./routes/orders')); // <-- Add this line
 app.use('/api/messages', require('./routes/messages')); // <-- And this line
+app.use('/api/payments', require('./routes/payments')); // <-- ADD THIS LINE
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
