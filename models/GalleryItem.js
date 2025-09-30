efd484c94de437b5a4679bc76c2cb393a7040b0e
Kiyoshiakira <shaunessy24@gmail.com>
2025-09-30 08:45:53 -0500

    Remove redundant mongoose schema definition

--- models/GalleryItem.js
+++ models/GalleryItem.js
@@ -8,14 +8,4 @@ const galleryItemSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now }
 });
 
-const mongoose = require('mongoose');
-
-const galleryItemSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  image: String, // URL or file path
-  uploadedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  createdAt: { type: Date, default: Date.now }
-});
-
 module.exports = mongoose.model('GalleryItem', galleryItemSchema);
